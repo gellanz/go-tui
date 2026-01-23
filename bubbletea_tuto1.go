c9da26ae8ce9ac01ab3ec33b981b3a8e351d9e5d
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2026-01-22 22:40:06 -0600

    tutorial 1 bubble  tea

---
+++ bubbletea_tuto1.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Buy carrots,", "Buy onions", "Buy tomatoes"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Market list:"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit\n"
+	return s
+}
+
+func main() {
+	fmt.Println("Hello world")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There has been an error: %v", err)
+		os.Exit(1)
+	}
+}
